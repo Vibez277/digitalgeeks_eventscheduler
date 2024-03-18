54ac3071a931f776193e9d927e7ff0501adb76b6
Vibez277 <109670585+Vibez277@users.noreply.github.com>
2024-03-19 00:59:46 +0200

    Update AuthContext.tsx

--- frontend/components/contexts/AuthContext.tsx
+++ frontend/components/contexts/AuthContext.tsx
@@ -18,28 +18,26 @@ export const AuthContext = createContext<auth_context|null>(null);
 const AuthContextProvider = (props: Props) => {
     const [isAuthorized,setIsAuthorized] = useState(false);
     const [user,setUser] = useState(false);
-    useEffect(()=>{
-        async function checkAuth(){
-            try {
-              const res = await fetch("https://digitalgeeks-eventscheduler.onrender.com/api/", {
-                method: "GET",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                credentials: "include",
-              });
-              const { message, success,user } = await res.json();
-              if (success) {
-                setIsAuthorized(true);
-                setUser(user)
-                return;
-              }
-            } catch (error) {
-              console.error(error);
-            }
+    async function checkAuth(){
+        try {
+          const res = await fetch("https://digitalgeeks-eventscheduler.onrender.com/api/", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          });
+          const { message, success,user } = await res.json();
+          if (success) {
+            setIsAuthorized(true);
+            setUser(user)
+            return;
           }
-          checkAuth();
-    },[])
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      checkAuth();
   return (
     <AuthContext.Provider value={{isAuthorized,setIsAuthorized,user}}>
         {
